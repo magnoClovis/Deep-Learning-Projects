5663b70a888522507f2c8d2762c0a372bd5887a2
Clovis Magno <clovism45@gmail.com>
2021-06-06 01:08:41 +0200

    Divided Dropdown and Options in two functios
    Created new file named options responsible for getting the available options from the dataset and splitted it from the old file (dropdown) where it was located together with the dropdown functions.

--- Adult Salary - Form/dropdown.py
+++ Adult Salary - Form/dropdown.py
@@ -1,99 +1,214 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun  5 23:20:08 2021
+Created on Sun Jun  6 00:25:58 2021
 
 @author: clovi
 """
-
-
-# Import module
 from tkinter import *
-import pandas as pd
-import numpy as np
+import options
 
-df = pd.read_csv('adult.csv')
+workclass, education, marital, occupation, relationship, race, sex, country = options.clean(options.get_options)
 
-
-# Getting possible options
-
-def get_options(df):
-    workclass = list(df['workclass'].unique())
-    workclass.remove(' ?')
-    workclass.append('Other')
+def create_root():
+    # Create object for dropdown
+    global root 
+    root = Tk()
+      
+    # Size
+    root.geometry("200x200")
     
-    education = list(df['education'].unique())
-    
-    marital = list(df['marital-status'].unique())
-
-    occupation = list(df['occupation'].unique())
-    occupation.remove(' ?')
-    occupation.append('Other')
-    
-    relationship = list(df['relationship'].unique())
-    
-    race = list(df['race'].unique())
-    
-    sex = list(df['sex'].unique())
-    
-    country = list(df['native-country'].unique())
-    country.remove(' ?')
-    country.append('Other')
-    
-
-    return (workclass, education, marital, occupation, relationship, race, sex, country)
-
-all_options = get_options(df)
-all_options_list=[]  
-    instance_keys = []
-    for i in range(len(instance_list)):
-        instance_std = ((instance_list[i][0].replace("-","")).replace(" ","")).lower()
-        instance_keys.append(instance_std)
-
-# Create object
-root = Tk()
-  
-# Adjust size
-root.geometry("200x200")
-  
-# Change the label text
-def show():
-    label.config(text = clicked.get())
-  
-# Dropdown menu options
-options = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-]
-  
-# datatype of menu text
-selected = StringVar()
+    # Datatype of menu text
+    global selected 
+    selected = StringVar()
 
 
-# initial menu text
-selected.set( "Monday" )
-  
+# Saving the selected option
 def change_dropdown(*args):
     global dropdown
     dropdown = str(selected.get())
+
+def drop_workclass():
+    create_root()
+    # Create Label
+    label = Label(root, text = "Workclass\n")
+    label.pack()
     
-# Create Dropdown menu
-drop = OptionMenu(root, selected, *workclass)
-drop.pack()
+    # Initial menu text
+    selected.set( "Select an Option" )
+      
+    # Create Dropdown menu
+    drop = OptionMenu(root, selected, *workclass)
+    drop.pack()
+    
+    # Create button, it will change label text
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+    # Execute tkinter
+    root.mainloop()
+    return dropdown
+
+def drop_education():
+    create_root()
+    label = Label(root, text = "Education\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, *education)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+def drop_marital():
+    create_root()
+    label = Label(root, text = "Marital Status\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, *marital)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+def drop_occupation():
+    create_root()
+    label = Label(root, text = "Occupation\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, *occupation)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+def drop_relationship():
+    create_root()
+    label = Label(root, text = "Relationship\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, *relationship)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+def drop_race():
+    create_root()
+    label = Label(root, text = "Race\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, *race)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
 
 
-# Create button, it will change label text
-button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+def drop_sex():
+    create_root()
+    label = Label(root, text = "Sex\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
 
-# Create Label
-label = Label(root, text = " ")
-label.pack()
-  
-  
-selected.trace('w', change_dropdown)
-# Execute tkinter
-root.mainloop()
+    drop = OptionMenu(root, selected, *sex)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+def drop_race():
+    create_root()
+    label = Label(root, text = "Race\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, *race)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+def drop_country():
+    create_root()
+    label = Label(root, text = "Country\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, *country)
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+def drop_exit():
+    create_root()
+    label = Label(root, text = "Exit the program?\n")
+    label.pack()
+    
+    selected.set( "Select an Option" )
+
+    drop = OptionMenu(root, selected, "Yes", "No")
+    drop.pack()
+    
+    button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
+      
+    selected.trace('w', change_dropdown)
+
+    root.mainloop()
+    return dropdown
+
+
+workclass_op = drop_workclass()
+education_op = drop_education()
+marital_op = drop_marital()
+occupation_op = drop_occupation()
+relationship_op = drop_relationship()
+race_op = drop_race()
+sex_op = drop_sex()
+country_op = drop_country()
+exit_op = drop_exit()
---
+++ Adult Salary - Form/options.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  5 23:20:08 2021
+
+@author: clovi
+"""
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('adult.csv')
+
+
+# Getting the possible options
+
+def get_options(df):
+    workclass = list(df['workclass'].unique())
+    workclass.remove(' ?')
+    workclass.append(' Other')
+    
+    education = list(df['education'].unique())
+    
+    marital = list(df['marital-status'].unique())
+
+    occupation = list(df['occupation'].unique())
+    occupation.remove(' ?')
+    occupation.append(' Other')
+    
+    relationship = list(df['relationship'].unique())
+    
+    race = list(df['race'].unique())
+    
+    sex = list(df['sex'].unique())
+    
+    country = list(df['native-country'].unique())
+    country.remove(' ?')
+    country.append(' Other')
+
+    return (workclass, education, marital, occupation, relationship, race, sex, country)
+
+def clean(get_options):
+    workclass=[]
+    education=[]
+    marital=[]
+    occupation=[]
+    relationship=[]
+    race=[]
+    sex=[]
+    country=[]
+    
+    classes = {0:workclass, 1:education, 2:marital, 3:occupation, 4:relationship, 5:race, 6:sex, 7:country}
+    
+    all_options = get_options(df)
+    for i in range(len(all_options)):
+        for j in range(len(all_options[i])):
+            options_std = (all_options[i][j].replace("-"," "))
+            classes[i].append(options_std)
+            
+    return workclass, education, marital, occupation, relationship, race, sex, country
+
+workclass, education, marital, occupation, relationship, race, sex, country = clean(get_options)
+
