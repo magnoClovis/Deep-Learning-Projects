ddca34d464fccdbeb51a4ac082020ecdf2bb2533
Clovis Magno <clovism45@gmail.com>
2021-06-05 23:59:23 +0200

    Creating lists of options
    Using the dataset information to generate the possible options fro the dropdown

--- Adult Salary - Form/dropdown.py
+++ Adult Salary - Form/dropdown.py
@@ -8,16 +8,56 @@ Created on Sat Jun  5 23:20:08 2021
 
 # Import module
 from tkinter import *
-  
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('adult.csv')
+
+
+# Getting possible options
+
+def get_options(df):
+    workclass = list(df['workclass'].unique())
+    workclass.remove(' ?')
+    workclass.append('Other')
+    
+    education = list(df['education'].unique())
+    
+    marital = list(df['marital-status'].unique())
+
+    occupation = list(df['occupation'].unique())
+    occupation.remove(' ?')
+    occupation.append('Other')
+    
+    relationship = list(df['relationship'].unique())
+    
+    race = list(df['race'].unique())
+    
+    sex = list(df['sex'].unique())
+    
+    country = list(df['native-country'].unique())
+    country.remove(' ?')
+    country.append('Other')
+    
+
+    return (workclass, education, marital, occupation, relationship, race, sex, country)
+
+all_options = get_options(df)
+all_options_list=[]  
+    instance_keys = []
+    for i in range(len(instance_list)):
+        instance_std = ((instance_list[i][0].replace("-","")).replace(" ","")).lower()
+        instance_keys.append(instance_std)
+
 # Create object
 root = Tk()
   
 # Adjust size
-root.geometry( "200x200" )
+root.geometry("200x200")
   
 # Change the label text
 def show():
-    label.config( text = clicked.get() )
+    label.config(text = clicked.get())
   
 # Dropdown menu options
 options = [
@@ -31,30 +71,29 @@ options = [
 ]
   
 # datatype of menu text
-clicked = StringVar()
+selected = StringVar()
 
 
 # initial menu text
-clicked.set( "Monday" )
+selected.set( "Monday" )
   
 def change_dropdown(*args):
     global dropdown
-    dropdown = str(clicked.get())
-    print(dropdown)
+    dropdown = str(selected.get())
     
 # Create Dropdown menu
-drop = OptionMenu( root , clicked , *options )
+drop = OptionMenu(root, selected, *workclass)
 drop.pack()
 
 
 # Create button, it will change label text
-button = Button( root , text = "OK" , command = root.destroy).pack(pady=20)
+button = Button(root, text = "OK", command = root.destroy).pack(pady=20)
 
 # Create Label
-label = Label( root , text = " " )
+label = Label(root, text = " ")
 label.pack()
   
   
-clicked.trace('w', change_dropdown)
+selected.trace('w', change_dropdown)
 # Execute tkinter
 root.mainloop()
